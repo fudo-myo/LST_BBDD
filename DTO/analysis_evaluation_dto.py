78fbb78ca265aa5ff99052e281bf8eb6ce722b6a
LEAN00\fjromero <fjromero@enaire.es>
2020-11-03 15:22:15 +0100

    Comments DTO object.

--- DTO/analysis_evaluation_dto.py
+++ DTO/analysis_evaluation_dto.py
@@ -1,14 +1,14 @@
 """
 This class is the transfer object of the Analysis Evaluation table,
 it contains the following parameters:
-:param id_analysis_evaluation: primary identifier of the table
-:type Integer
-:param id_lst_r1_data_check_plot: TODO include description
-:type Integer
-:param parameter_description: TODO include description
-:type String
-:param parameter_value: TODO include description
-:type Double
+:param: id_analysis_evaluation: primary identifier of the table
+:type: Integer
+:param: id_lst_r1_data_check_plot: TODO include description
+:type: Integer
+:param: parameter_description: TODO include description
+:type: String
+:param: parameter_value: TODO include description
+:type: Double
 """
 
 
@@ -16,7 +16,7 @@ class AnalysisEvaluationDto:
     """
     class constructor that allows instantiating a class object with
     all null attributes
-    :return an instance of the class AnalysisEvaluationDto
+    :return: an instance of the class AnalysisEvaluationDto
     """
 
     def __init__(self, id_analysis_evaluation=None, id_lst_r1_data_check_plot=None, parameter_description=None,
@@ -64,12 +64,12 @@ def create_analysis_evaluation(id_analysis_evaluation, id_lst_r1_data_check_plot
     """
     This method create a DTO for Analysis Evaluation and set every field with
     the paremeters given as arguments
-    :param id_analysis_evaluation: primary identifier of the table
-    :param id_lst_r1_data_check_plot: TODO include description
-    :param parameter_description: TODO include description
-    :param parameter_value: TODO include description
+    :param: id_analysis_evaluation: primary identifier of the table
+    :param: id_lst_r1_data_check_plot: TODO include description
+    :param: parameter_description: TODO include description
+    :param: parameter_value: TODO include description
 
-    :return AnalysisEvaluationDto
+    :return: AnalysisEvaluationDto
     """
     dto = AnalysisEvaluationDto()
     dto.id_analysis_evaluation = id_analysis_evaluation
